417d96e7be2107df95711016e45a8c70adeb1a58
derlancruz <derlancruz@users.noreply.github.com>
2023-06-18 14:52:28 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio); 
+   
+  if (elemento && elemento.localName === 'audio' ) {     
+         elemento.play();
+      } else {
+         console.log('Elemento ou seletor inválido');
+      }
+   
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//percorre toda lista
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+   tecla.onclick = function () { tocaSom(idAudio) };
+   
+   tecla.onkeydown = function (evento) {
+      if(evento.code === "Enter" || evento.code === "Space") {
+      tecla.classList.add('ativa');
+      }
+   }
+
+   tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+   }
+}
